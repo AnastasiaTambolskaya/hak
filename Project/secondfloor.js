7e239336d0ffb7973747ca28547c33caf3150eb0
AnastasiaTambolskaya <147328653+AnastasiaTambolskaya@users.noreply.github.com>
2023-10-26 21:12:09 +0300

    create new files

---
+++ Project/secondfloor.js
@@ -0,0 +1,6 @@
+document.getElementById("myButton").addEventListener("click", function() {
+    window.location.href = "secondfloor_3_floor.html";
+  });
+  document.getElementById("myButton2").addEventListener("click", function() {
+    window.location.href = "firstfloor.html";
+  });
